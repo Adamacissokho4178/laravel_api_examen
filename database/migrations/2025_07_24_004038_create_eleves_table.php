2b58be24d31c7848f3fd7afc6a7f1f6be455e5e3
Adamacissokho4178 <Adamacissokho4178@gmail.com>
2025-07-24 14:21:55 +0000

    Ajout de la migration create_eleves_table

--- database/migrations/2025_07_24_004038_create_eleves_table.php
+++ database/migrations/2025_07_24_004038_create_eleves_table.php
@@ -13,7 +13,17 @@ return new class extends Migration
     {
         Schema::create('eleves', function (Blueprint $table) {
             $table->id();
+            $table->string('prenom');
+            $table->string('nom');
+            $table->string('email')->unique();
+            $table->date('date_naissance');
+            $table->unsignedBigInteger('classe_id')->nullable();
+            $table->string('chemin_document')->nullable(); // justificatif PDF ou autre
+            $table->unsignedBigInteger('utilisateur_id')->nullable(); // identifiant user lié
             $table->timestamps();
+
+            $table->foreign('classe_id')->references('id')->on('classes')->onDelete('set null');
+            $table->foreign('utilisateur_id')->references('id')->on('users')->onDelete('set null');
         });
     }
 
